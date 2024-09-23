c749e53e175370dd8304d188a1251a01bf70cd2b
Reverier-Xu <reverier.xu@stu.xidian.edu.cn>
2024-09-23 14:48:57 +0800

    :sparkles: add typst community plugin

--- lua/community.lua
+++ lua/community.lua
@@ -17,5 +17,6 @@ return {
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.xml" },
   { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.typst" },
   -- import/override with your plugins folder
 }
