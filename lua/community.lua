cb86a5613a1d2729db7a9bcec1367ecb72b5da75
Reverier-Xu <reverier.xu@stu.xidian.edu.cn>
2024-10-31 09:47:30 +0800

    :sparkles: add neominimap

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.split-and-window.neominimap-nvim" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.lua" },
---
+++ lua/plugins/neominimap.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "Isrothy/neominimap.nvim",
+    opts = {
+      float = {
+        minimap_width = 16, ---@type integer
+
+        --- If set to nil, there is no maximum height restriction
+        --- @type integer
+        max_minimap_height = nil,
+
+        margin = {
+          right = 0, ---@type integer
+          top = 0, ---@type integer
+          bottom = 0, ---@type integer
+        },
+        z_index = 1, ---@type integer
+
+        --- Border style of the floating window.
+        --- Accepts all usual border style options (e.g., "single", "double")
+        --- @type string | string[] | [string, string][]
+        window_border = "none",
+      },
+      click = {
+        -- Enable mouse click on minimap
+        enabled = true, ---@type boolean
+        -- Automatically switch focus to minimap when clicked
+        auto_switch_focus = false, ---@type boolean
+      },
+    },
+  },
+}
