3fee5de32fe92f107cd1c0ff1fdbb0bf42acafd4
Reverier-Xu <reverier.xu@woooo.tech>
2024-11-03 19:44:06 +0800

    :bug: fix Isrothy/neominimap.nvim#177

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -1,32 +1,38 @@
 return {
+  ---@module "neominimap.config.meta"
   {
     "Isrothy/neominimap.nvim",
-    opts = {
-      float = {
-        minimap_width = 16, ---@type integer
+    init = function()
+      vim.opt.wrap = false
+      vim.opt.sidescrolloff = 36
+      ---@type Neominimap.UserConfig
+      vim.g.neominimap = {
+        float = {
+          minimap_width = 16, ---@type integer
 
-        --- If set to nil, there is no maximum height restriction
-        --- @type integer
-        max_minimap_height = nil,
+          --- If set to nil, there is no maximum height restriction
+          --- @type integer
+          max_minimap_height = nil,
 
-        margin = {
-          right = 0, ---@type integer
-          top = 0, ---@type integer
-          bottom = 0, ---@type integer
+          margin = {
+            right = 0, ---@type integer
+            top = 0, ---@type integer
+            bottom = 0, ---@type integer
+          },
+          z_index = 1, ---@type integer
+
+          --- Border style of the floating window.
+          --- Accepts all usual border style options (e.g., "single", "double")
+          --- @type string | string[] | [string, string][]
+          window_border = "none",
         },
-        z_index = 1, ---@type integer
-
-        --- Border style of the floating window.
-        --- Accepts all usual border style options (e.g., "single", "double")
-        --- @type string | string[] | [string, string][]
-        window_border = "none",
-      },
-      click = {
-        -- Enable mouse click on minimap
-        enabled = true, ---@type boolean
-        -- Automatically switch focus to minimap when clicked
-        auto_switch_focus = false, ---@type boolean
-      },
-    },
+        click = {
+          -- Enable mouse click on minimap
+          enabled = true, ---@type boolean
+          -- Automatically switch focus to minimap when clicked
+          auto_switch_focus = false, ---@type boolean
+        },
+      }
+    end,
   },
 }
